f75d6d63f18b1d86c75cf9ae5e7bad3ff35bf26f
kayquepiton <kayquepiton@gmail.com>
2024-03-08 17:36:51 -0300

    contato

--- PururucaSystem/src/app/validators/earTag.validator.ts
+++ PururucaSystem/src/app/validators/earTag.validator.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { DataBaseService } from '../services/data-base.service';
-=======
-import { AbstractControl, ValidatorFn } from '@angular/forms';
-import { DataBaseService } from '../data-base.service';
->>>>>>> 70e1b1f067902cfa8bdf0ce640c04550abd80710
 import { map } from 'rxjs/operators';
 
 export function earTagValidator(dataBaseService: DataBaseService): ValidatorFn {
